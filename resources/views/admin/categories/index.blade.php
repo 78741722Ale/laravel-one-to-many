ac038c7b71fdd7d667cb64bbb0582f28708ea454
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-25 11:17:52 +0200

    Aggiornato componente per le categorie

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,29 +1,27 @@
 @extends('layouts.admin')
 
-
 @section('content')
-
-
-
 
 @include('partials.session_message')
 @include('partials.errors')
 
 <div class="container">
-    <h1 class="my-3">All Categories</h1>
+    <h1 class="my-3">Categorie</h1>
     <div class="row">
+        <!-- Colonna per l'aggiunta di una categoria -->
         <div class="col pe-5">
             <form action="" method="post" class="d-flex align-items-center">
                 @csrf
                 <div class="input-group mb-3">
-                    <input type="text" name="name" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Add</button>
+                    <input type="text" name="name" class="form-control" placeholder="Aggiungi Categoria" aria-label="Aggiungi Categoria" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Aggiungi</button>
                 </div>
             </form>
         </div>
+        <!-- Colonna con le categorie -->
         <div class="col">
-
             <table class="table table-striped table-inverse table-responsive">
+                <!-- Intestazione Tabella -->
                 <thead class="thead-inverse">
                     <tr>
                         <th>ID</th>
@@ -33,44 +31,45 @@
                         <th>Actions</th>
                     </tr>
                 </thead>
+                <!-- Corpo della tabella -->
                 <tbody>
                     @forelse($cats as $category)
                     <tr>
+                        <!-- Id -->
                         <td scope="row">{{$category->id}}</td>
+                        <!-- Slug -->
                         <td>
                             <form id="category-{{$category->id}}" action="{{route('admin.categories.update', $category->slug)}}" method="post">
                                 @csrf
                                 @method('PATCH')
                                 <input class="border-0 bg-transparent" type="text" name="name" value="{{$category->name}}">
                             </form>
-
                         </td>
+                        <!-- Slug -->
                         <td>{{$category->slug}}</td>
                         <td><span class="badge badge-info bg-dark">{{count($category->posts)}}</span></td>
+                        <!-- Buttons edit e cancella -->
                         <td>
-                            <button form="category-{{$category->id}}" type="submit" class="btn btn-primary">Update</button>
+                            <!-- Button edit -->
+                            <button form="category-{{$category->id}}" type="submit" class="btn btn-primary">Aggiorna</button>
+                            <!-- Button Delete -->
                             <form action="{{route('admin.categories.destroy', $category->slug)}}" method="post">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger text-white">Delete</button>
+                                <button type="submit" class="btn btn-danger text-white">Cancella</button>
                             </form>
-
                         </td>
                     </tr>
+                    <!-- In caso di vuoto -->
                     @empty
                     <tr>
-                        <td scope="row">No categories! Add your first category.</td>
-
+                        <td scope="row">Non ci sono categorie</td>
                     </tr>
                     @endforelse
                 </tbody>
             </table>
-
-
         </div>
-
     </div>
 </div>
-
 
 @endsection
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,15 +6,15 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }} {{ __('You are logged in!') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
+                    <!-- Button per il crea -->
                     <div>
-                        <a href="{{route('admin.posts.create')}}" class="btn btn-primary">Create Post</a>
+                        <a href="{{route('admin.posts.create')}}" class="btn btn-primary">Crea un Post</a>
                     </div>
                 </div>
             </div>
